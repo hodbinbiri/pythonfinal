1fb8d7245748ad0de3bdd25db4e623e47b73edb6
Hodbin <78754522+hodbinbiri@users.noreply.github.com>
2022-04-11 21:39:54 +0300

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,14 @@
+dicty = {1:"emre", 2:"arif", 3:"sevil", 4:"ayşe"}
+print(dicty)
+
+print(dicty[1])
+print(dicty.keys())
+print(dicty.values())
+
+print(len(dicty))
+dicty[5]="ahmet"
+print(dicty)
+
+
+dicty.pop(3)
+print(dicty)
